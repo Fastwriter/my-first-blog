3230d334b338dc7b1c80c3d059f8ba1d0cd76877
fastwriter <demeuov.daulet.nis.ura@gmail.com>
2018-12-07 00:23:04 +0600

    Testing#5: Added pagination for blog's posts

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-from django.conf.urls import url
 from . import views
 
 urlpatterns = [
@@ -8,10 +7,3 @@ urlpatterns = [
     path('post/new/', views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
-
-urlpatterns = [
-    url(r'^category/(?P.*?)/*$',    views.list,             name='entry_list'),
-    url(r'^author/(?P.*?)/*$',      views.list,             name='entry_list'),
-    url(r'^date/(?P.*?)/*$',            views.list,             name='entry_list'),
-    url(r'^$',                                views.list,             name='entry_list'),
-   ]
--- blog/views.py
+++ blog/views.py
@@ -4,9 +4,23 @@ from .models import Post
 from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
 from django.shortcuts import redirect
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts_list = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    paginator = Paginator(posts_list, 25) # Show 25 contacts per page
+
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    context={
+     "object_list":posts,
+     "title":"List"
+    }
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
@@ -42,51 +56,4 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
-
-def list(request, category=None, username=None, date=None):
-    """
-    List all entries, paginated
-    """
-    template_name = 'list.html'
-    context = {}
-    per_page = 5
-    page = INT(request.GET.get('page', '1'))
-
-    if category:
-        context['category'] = category
-        grouped_list = entries_by_category(request, category)
-    elif username:
-        context['author'] = username
-        try:
-            user = User.objects.get(username=username)
-            grouped_list = Post.objects.filter(author=user.id).order_by('-created_at')
-        except ObjectDoesNotExist:
-            grouped_list = None
-    elif date:
-        context['date'] = date
-        grouped_list = Post.objects.filter(created_at__startswith=date, publish=True).order_by('-created_at')
-    else:
-        grouped_list = Post.objects.filter(publish=True).order_by('-created_at')
-
-    for entry in grouped_list:
-        entry.category_list = Category.objects.filter(postcategory__post__pk=entry.id)
-        entry.comments = Comment.objects.filter(post=entry.id)
-
-    total_entries = grouped_list.count()
-    total_pages = (total_entries/per_page)+1
-    context['page_range'] = range(1, total_pages+1)
-
-    offset = (page * per_page) - per_page
-    limit = offset + per_page
-    entry_list = grouped_list[offset:limit]
-    context['entry_list'] = entry_list
-
-    return render_to_response(template_name, context, context_instance=RequestContext(request))
-
-def entries_by_category(request, category):
-    try:
-        post_category = Category.objects.get(slug=category)
-    except ObjectDoesNotExist:
-        post_category = None
-    if post_category: entry_list = Post.objects.filter(postcategory__category=post_category.id, publish=True)
-    return entry_list    
+    
