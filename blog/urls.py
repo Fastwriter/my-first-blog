e4448c7bc245868661b3cba8813822d784a1ac66
fastwriter <demeuov.daulet.nis.ura@gmail.com>
2018-12-06 19:18:38 +0600

    Testing: Adding pagination for blog's posts

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new/', views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path(r'^category/(?P.*?)/*$',    views.list,             name='entry_list'),
+    path(r'^author/(?P.*?)/*$',      views.list,             name='entry_list'),
+    path(r'^date/(?P.*?)/*$',            views.list,             name='entry_list'),
+    path(r'^$',                                views.list,             name='entry_list'),
 ]
