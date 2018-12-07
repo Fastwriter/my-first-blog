ccc82a4fab30e493ee729c25b161ce0898748f83
fastwriter <demeuov.daulet.nis.ura@gmail.com>
2018-12-07 19:58:51 +0600

    Added pagination for blog

--- blog/views.py
+++ blog/views.py
@@ -7,14 +7,12 @@ from django.shortcuts import redirect
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-     return render(request, 'blog/post_list.html', {'posts': posts})
-     posts_list = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date').all()
-     paginator = Paginator(posts_list, 5)  # Show 55 posts per page
+    posts_list = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date').all()
+    paginator = Paginator(posts_list, 5)  # Show 55 posts per page
 
-     page = request.GET.get('page')
-     posts = paginator.get_page(page)
-     return render(request, 'blog/post_list.html', {'posts': posts})
+    page = request.GET.get('page')
+    posts = paginator.get_page(page)
+    return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
 	Post.objects.get(pk=pk)
