6617bc9da71d26c3b922ecb03e27bd8923c93500
Fastwriter <demeuov.daulet.nis.ura@gmail.com>
2018-12-09 15:23:57 +0600

    Completed: blog with finished Django practice task

--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,20 @@
-from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .forms import PostForm
-from django.shortcuts import redirect
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.db.models import Q
 
 def post_list(request):
     posts_list = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date').all()
-    paginator = Paginator(posts_list, 5)  # Show 55 posts per page
+    
+    query = request.GET.get("q")
+    if query:
+        posts_list = posts_list.filter(
+            Q(title__icontains = query) |
+            Q(text__icontains = query)
+            ).distinct()
+    paginator = Paginator(posts_list, 3)
 
     page = request.GET.get('page')
     posts = paginator.get_page(page)
@@ -47,4 +53,15 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_delete(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post.delete()
+            return redirect('post_list')
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_delete.html', {'form': form})
     
---
+++ moivenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/demeuovdaulet/Desktop/djangogirls/moivenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
